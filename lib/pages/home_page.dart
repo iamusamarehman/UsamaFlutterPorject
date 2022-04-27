4c2c8abfbb8c970ffbf8df7176ad0021e53579b6
usama rehman <iusamarehman96@gmail.com>
2022-04-27 14:49:21 +0500

    load data in json done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,33 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:master_flutter/models/catalog.dart';
 import 'package:master_flutter/widgets/drawer.dart';
 
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int Days = 30;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodeData = jsonDecode(catalogJson);
+    var productData = decodeData["products"];
+    print(productData);
+  }
+
   final String name = "hello world";
 
   @override
